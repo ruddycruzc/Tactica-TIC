7eceecf932c3c21b8dc4d818826bdbcc0e9ff966
Seni99 <inesarriazu@gmail.com>
2026-06-01 13:13:46 +0200

    Add contact and newsletter form components with associated styles and tests

---
+++ src/app/components/forms/newsletter-form/newsletter-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newsletter-form',
+  imports: [],
+  templateUrl: './newsletter-form.html',
+  styleUrl: './newsletter-form.css',
+})
+export class NewsletterForm {
+
+}
