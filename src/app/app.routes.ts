d29f2ac66e7f2ef67005d21f9fa8ac3dc2133e50
Ruddy <ruddy391@gmail.com>
2026-06-01 13:11:49 +0200

    routing between navigation pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Projects } from './pages/projects/projects';
+import { About } from './pages/about/about';
+import { Careers } from './pages/careers/careers';
+import { Blog } from './pages/blog/blog';
+import { Contact } from './pages/contact/contact';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'projects',
+    component: Projects
+  },
+  {
+    path: 'about',
+    component: About
+  },
+  {
+    path: 'careers',
+    component: Careers
+  },
+  {
+    path: 'blog',
+    component: Blog
+  },
+  {
+    path: 'contact',
+    component: Contact
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
