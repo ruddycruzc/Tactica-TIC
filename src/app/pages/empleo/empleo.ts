0711578ff864facb5a9be44be8b59ff3bf56b5b6
Ruddy <ruddy391@gmail.com>
2026-06-01 12:10:13 +0200

    deleted all files and branchs

--- src/app/pages/empleo/empleo.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-empleo',
-  imports: [],
-  templateUrl: './empleo.html',
-  styleUrl: './empleo.css',
-})
-export class Empleo {}
--- src/app/pages/lo-que-hacemos/lo-que-hacemos.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-lo-que-hacemos',
-  imports: [],
-  templateUrl: './lo-que-hacemos.html',
-  styleUrl: './lo-que-hacemos.css',
-})
-export class LoQueHacemos {}
