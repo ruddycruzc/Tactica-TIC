d7db39face67424589d57c2c8ad6a65b5dd7f66f
Ruddy <ruddy391@gmail.com>
2026-06-02 15:35:55 +0200

    add:navbar link navigation working

--- src/app/components/layout/navbar/navbar.ts
+++ src/app/components/layout/navbar/navbar.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
