713fbde9891f11982a68bc7c4f4aa7ef5ae4ec3d
Ruddy <ruddy391@gmail.com>
2026-06-02 12:26:19 +0200

    add files on home.html

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../../components/layout/navbar/navbar';
+import { Body } from '../../components/layout/body/body';
+import { Footer } from '../../components/layout/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [Navbar, Body, Footer],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
 export class Home {}
