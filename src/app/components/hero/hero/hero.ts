84f28aebd2b97c5504ddb07d858c716d4af637ae
Ruddy <ruddy391@gmail.com>
2026-06-03 19:49:01 +0200

    refactor: remove unused stylesheet link and update imports in hero component

--- src/app/components/hero/hero/hero.ts
+++ src/app/components/hero/hero/hero.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './hero.html',
-  styleUrl: './hero.css',
+  styleUrl: './hero.css'
 })
 export class Hero {}
