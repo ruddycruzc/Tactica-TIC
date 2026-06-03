8f87799bd2907b75b67bc7ceaf57ef43cd2e3e52
Ruddy <ruddy391@gmail.com>
2026-06-03 12:42:09 +0200

    feat: implement navbar and icon button components with navigation functionality

--- src/app/components/layout/navbar/navbar.ts
+++ src/app/components/layout/navbar/navbar.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import { IconButton } from '../../buttons/iconbutton/iconbutton';
+
+import { NavItem } from '../../../interfaces/nav-item.interface';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, IconButton ],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrl: './navbar.css'
 })
+
+
 export class Navbar {
+
+  navigationItems: NavItem[] = [
+    {
+      label: 'Cómo pensamos',
+      route: '/about'
+    },
+    {
+      label: 'Proyectos',
+      route: '/projects'
+    },
+    {
+      label: 'Contacto',
+      route: '/contact'
+    },
+    {
+      label: 'Blog',
+      route: '/blog'
+    },
+    {
+      label: 'Empleos',
+      route: '/careers'
+    }
+  ];
+
+  onSearchClick(): void {
+    console.log('Buscar');
+  }
 
 }
