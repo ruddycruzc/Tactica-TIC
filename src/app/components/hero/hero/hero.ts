cc8750ac772764539d431f7ed43eff382331b0a2
marciicab <marciicab@gmail.com>
2026-06-05 00:43:02 +0200

    animacion en el hero

--- src/app/components/hero/hero/hero.ts
+++ src/app/components/hero/hero/hero.ts
@@ -9,6 +9,16 @@ import { RouterLink } from '@angular/router';
   styleUrl: './hero.css',
 })
 export class Hero {
+  readonly wavePaths = Array.from({ length: 38 }, (_, index) => {
+    const y = 58 + index * 5.6;
+    const lift = index - 18.5;
+
+    return `M -70 ${y}
+      C 90 ${y - 96 - lift * 1.2}, 220 ${y - 92}, 360 ${y - 18}
+      C 520 ${y + 66 + lift * 0.9}, 610 ${y + 74}, 760 ${y + 8}
+      C 930 ${y - 68 - lift * 0.8}, 1050 ${y - 62}, 1280 ${y + 10}`;
+  });
+
   cursorX = 50;
   cursorY = 50;
   isPointerActive = false;
