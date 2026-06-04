7e580a3a9596d76e124ab5b9398f6d085dc810dd
Ruddy <ruddy391@gmail.com>
2026-06-04 11:54:00 +0200

    add: cards-carrousel, case-studies sections on home.html

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,6 +4,8 @@ import { Hero } from '../../components/hero/hero/hero';
 import { InfoRightImage } from '../../components/info-blocks/info-right-image/info-right-image';
 import { ContactForm } from '../../components/forms/contact-form/contact-form';
 import { VideoHero } from '../../components/hero/video-hero/video-hero';
+import { CaseStudiesSection } from "../../components/sections/case-studies-section/case-studies-section";
+import { CardsCarousel } from '../../components/carousel/cards-carousel/cards-carousel';
 
 @Component({
   selector: 'app-home',
@@ -12,8 +14,10 @@ import { VideoHero } from '../../components/hero/video-hero/video-hero';
     Hero,
     InfoRightImage,
     ContactForm,
-    VideoHero
-  ],
+    VideoHero,
+    CaseStudiesSection,
+    CardsCarousel
+],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
