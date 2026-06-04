50d1f96fe35e5942211d4b1253e5dc76df92adc0
marciicab <marciicab@gmail.com>
2026-06-05 00:46:44 +0200

    seccion FAQ

--- src/app/components/sections/faq-section/faq-section.ts
+++ src/app/components/sections/faq-section/faq-section.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-faq-section',
+  standalone: true,
   imports: [],
   templateUrl: './faq-section.html',
   styleUrl: './faq-section.css',
