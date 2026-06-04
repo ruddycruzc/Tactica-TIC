5037ce2959b0bbdd525ace60f2ccbdab44ff4508
Ruddy <ruddy391@gmail.com>
2026-06-04 11:35:14 +0200

    add section contact-form on home.html

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 
 import { Hero } from '../../components/hero/hero/hero';
 import { InfoRightImage } from '../../components/info-blocks/info-right-image/info-right-image';
+import { ContactForm } from '../../components/forms/contact-form/contact-form';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     Hero,
-    InfoRightImage
+    InfoRightImage,
+    ContactForm
   ],
   templateUrl: './home.html',
   styleUrl: './home.css'
