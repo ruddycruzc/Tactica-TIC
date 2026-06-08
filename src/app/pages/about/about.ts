27289c1e4afdb1a5de5700c860a1a649c2b7b0df
Seni99 <inesarriazu@gmail.com>
2026-06-08 11:25:04 +0200

    enhance about section styles and structure

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { AboutSection } from '../../components/sections/about-section/about-section';
+import { ClientsSection } from '../../components/sections/clients-section/clients-section';
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [AboutSection,ClientsSection],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
