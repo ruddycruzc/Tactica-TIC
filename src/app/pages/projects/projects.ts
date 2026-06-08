1b9f21a46ad0ec6c60d50a078f5e14a234083769
Seni99 <inesarriazu@gmail.com>
2026-06-08 11:55:18 +0200

    update projects section styles and structure, optimize HTML and CSS, and add new project images

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { ProjectsSection } from '../../components/sections/projects-section/projects-section';  
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [ProjectsSection],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
