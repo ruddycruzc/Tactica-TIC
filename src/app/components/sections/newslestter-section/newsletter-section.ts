4b4db35e20d309d3026b993f29a7ac44f22dffad
Seni99 <inesarriazu@gmail.com>
2026-06-09 12:06:19 +0200

    Refactor newsletter section to use dynamic rendering for hub items and add type safety with HubItem interface

--- src/app/components/sections/newslestter-section/newsletter-section.ts
+++ src/app/components/sections/newslestter-section/newsletter-section.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 import { AppButtonComponent } from '../../buttons/app-button/app-button.component';
+import { HubItem } from '../../../interfaces/newsletter-section.interface';
 
 @Component({
   selector: 'app-newsletter-section',
-  imports: [AppButtonComponent],
+  standalone: true,
+  imports: [AppButtonComponent, NgFor],
   templateUrl: './newsletter-section.html',
-  styleUrl: './newsletter-section.css',
+  styleUrl: './newsletter-section.css'
 })
 export class NewsletterSection {
-
+  readonly hubItems: HubItem[] = [
+    {
+      cssClass: 'item-linkedin',
+      title: 'Área Profesional',
+      description: 'Ofertas de empleo, artículos de software e ingeniería de vanguardia.',
+      buttonVariant: 'primary', // <-- TypeScript validará que es correcto
+      link: 'https://www.linkedin.com/company/tactica-tic/',
+      buttonText: 'Seguir en LinkedIn'
+    },
+    {
+      cssClass: 'item-instagram',
+      title: 'Cultura & Equipo',
+      description: 'Echa un vistazo a nuestros eventos, el día a día en la oficina y ambiente real.',
+      buttonVariant: 'secondary', // <-- TypeScript validará que es correcto
+      link: 'https://www.instagram.com/tactica_tic/',
+      buttonText: 'Ver en Instagram'
+    }
+  ];
 }
