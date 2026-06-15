5cc0b9a5292f40bd6e3904f28e640e3256d3e78c
Ruddy <ruddy391@gmail.com>
2026-06-15 12:43:19 +0200

    fix(careers): comment out final CTA section and add contact form

--- src/app/pages/careers/careers.ts
+++ src/app/pages/careers/careers.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { JobOpeningsSection } from '../../components/sections/job-openings-section/job-openings-section';
 import { FinalCtaSection } from '../../components/sections/final-cta-section/final-cta-section';
+import { ContactForm } from "../../components/forms/contact-form/contact-form";
 
 @Component({
   selector: 'app-careers',
-  imports: [JobOpeningsSection, FinalCtaSection],
+  imports: [JobOpeningsSection, FinalCtaSection, ContactForm],
   templateUrl: './careers.html',
   styleUrl: './careers.css',
 })
