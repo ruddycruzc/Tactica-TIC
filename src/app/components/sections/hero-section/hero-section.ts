63e0da7617f2baca5735c44de2b721e357c8c46e
marciicab <marciicab@gmail.com>
2026-06-16 09:21:06 +0200

    anadir componente de fondo aanimado y ponerlo en el home, cambiar peso del video aboutus

--- src/app/components/sections/hero-section/hero-section.ts
+++ src/app/components/sections/hero-section/hero-section.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
 import { Hero } from '../../hero/hero/hero';
+import { AnimatedBackground } from '../../media/animated-background/animated-background';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [Hero],
+  imports: [
+    AnimatedBackground,
+    Hero
+  ],
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.css',
 })
